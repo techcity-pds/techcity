250b0281e20cc758712bf91d3c39b5378af746e2
esipe <your_email@exemple.fr>
2020-10-19 12:27:48 +0200

    création du projet

---
+++ techcity/techcity-backend/src/test/java/fr/episen/ing2/pds/techcity/AppTest.java
@@ -0,0 +1,12 @@
+package fr.episen.ing2.pds.techcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
